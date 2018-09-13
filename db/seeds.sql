5eeafe425f7c75969dea82b50da9f3858c105ed8
GGA <geoffrey.godwin-austen@outlook.com>
2018-09-12 20:47:59 -0600

    inital files

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name) VALUES ("Momma's Breakfast Burger");
+INSERT INTO burgers (burger_name) VALUES ("Ken's Keto Burger");
+INSERT INTO burgers (burger_name) VALUES ("Vic's Vegan Burger");
